e1a6c67bf107daf8e14e72eb6cf2531827cf5c00
Paradox456 <islamannafi@gmail.com>
2026-03-06 22:23:06 -0500

    Fix bcrypt password length limit error

--- api/core/auth.py
+++ api/core/auth.py
@@ -13,7 +13,8 @@ ALGORITHM="HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES=60 * 24 * 7
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    if len(password.encode('utf-8')) > 72:
+        return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
