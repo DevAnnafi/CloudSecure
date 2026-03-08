17dbddb0dfc9318a61432785f065ded01efd4f95
Paradox456 <islamannafi@gmail.com>
2026-03-07 20:46:54 -0500

    Forget about the analytcis

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import Navbar from "@/components/Navbar"
-import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
